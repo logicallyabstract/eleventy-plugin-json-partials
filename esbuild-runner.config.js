a7a2c267e89f8b5546da9f2bae139c1230c234ca
AJ Klein <2043439+silenceisgolden@users.noreply.github.com>
2022-07-01 00:01:41 -0500

    Remove esbuild-runner config

--- esbuild-runner.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  type: 'bundle', // bundle or transform (see description above)
-  esbuild: {
-    // Any esbuild build or transform options go here
-    target: 'es6',
-
-    format: 'cjs',
-  },
-};
